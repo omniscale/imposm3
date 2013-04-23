3e2484db268b061579838afb97bc7276c2b17e7d
Oliver Tonnhofer <olt@bogosoft.com>
2013-04-23 22:02:27 +0200

    add LineStringWKB/PolygonWKB functions

--- geom/geom.go
+++ geom/geom.go
@@ -5,15 +5,45 @@ import (
 	"goposm/element"
 )
 
-func LineString(nodes []element.Node) []byte {
-	geos := gogeos.NewGEOS()
-	defer geos.Finish()
-
-	coordSeq := geos.CreateCoordSeq(uint32(len(nodes)), 2)
+func LineStringWKB(geos *gogeos.GEOS, nodes []element.Node) ([]byte, error) {
+	coordSeq, err := geos.CreateCoordSeq(uint32(len(nodes)), 2)
+	if err != nil {
+		return nil, err
+	}
+	// coordSeq inherited by LineString
 	for i, nd := range nodes {
 		coordSeq.SetXY(geos, uint32(i), nd.Long, nd.Lat)
 	}
-	geom := coordSeq.AsLineString(geos)
+	geom, err := coordSeq.AsLineString(geos)
+	if err != nil {
+		return nil, err
+	}
 	defer geos.Destroy(geom)
+	return geos.AsWKB(geom)
+}
+
+func PolygonWKB(geos *gogeos.GEOS, nodes []element.Node) ([]byte, error) {
+	coordSeq, err := geos.CreateCoordSeq(uint32(len(nodes)), 2)
+	if err != nil {
+		return nil, err
+	}
+	// coordSeq inherited by LineString, no destroy
+	for i, nd := range nodes {
+		err := coordSeq.SetXY(geos, uint32(i), nd.Long, nd.Lat)
+		if err != nil {
+			return nil, err
+		}
+	}
+	geom, err := coordSeq.AsLinearRing(geos)
+	if err != nil {
+		return nil, err
+	}
+	// geom inherited by Polygon, no destroy
+	geom = geos.CreatePolygon(geom, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer geos.Destroy(geom)
+
 	return geos.AsWKB(geom)
 }
--- geom/geom_test.go
+++ geom/geom_test.go
@@ -1,20 +1,59 @@
 package geom
 
 import (
+	"bytes"
+	"gogeos"
 	"goposm/element"
-	"regexp"
 	"testing"
 )
 
-func _TestLineString(t *testing.T) {
+func TestLineString(t *testing.T) {
 	nodes := make([]element.Node, 2)
 	nodes[0] = element.Node{Lat: 0, Long: 0}
 	nodes[1] = element.Node{Lat: 0, Long: 10}
-	wkt := LineString(nodes)
-	re := regexp.MustCompile("LINESTRING \\(0\\.0* 0\\.0*, 10\\.0* 0\\.0*\\)")
-	if !re.Match(wkt) {
+	geos := gogeos.NewGEOS()
+	defer geos.Finish()
+	wkt, err := LineStringWKB(geos, nodes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if bytes.Compare(wkt[0:2], []byte{0x1, 0x2}) != 0 {
 		t.Errorf("%#v", wkt)
 	}
+}
+
+func TestPolygon(t *testing.T) {
+	nodes := []element.Node{
+		element.Node{Lat: 0, Long: 0},
+		element.Node{Lat: 0, Long: 10},
+		element.Node{Lat: 10, Long: 10},
+		element.Node{Lat: 0, Long: 0},
+	}
+	geos := gogeos.NewGEOS()
+	defer geos.Finish()
+	wkt, err := PolygonWKB(geos, nodes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if bytes.Compare(wkt[0:2], []byte{0x1, 0x3}) != 0 {
+		t.Errorf("%#v", wkt)
+	}
+}
+
+func TestPolygonNotClosed(t *testing.T) {
+	nodes := []element.Node{
+		element.Node{Lat: 0, Long: 0},
+		element.Node{Lat: 0, Long: 10},
+		element.Node{Lat: 10, Long: 10},
+	}
+	geos := gogeos.NewGEOS()
+	defer geos.Finish()
+	_, err := PolygonWKB(geos, nodes)
+	if err == nil {
+		t.Fatal("no error")
+	}
+
 }
 
 func BenchmarkLineString(b *testing.B) {
@@ -23,8 +62,10 @@ func BenchmarkLineString(b *testing.B) {
 	for i := 0; i < size; i++ {
 		nodes[i] = element.Node{Lat: 0, Long: float64(i)}
 	}
+	geos := gogeos.NewGEOS()
+	defer geos.Finish()
 
 	for i := 0; i < b.N; i++ {
-		LineString(nodes)
+		LineStringWKB(geos, nodes)
 	}
 }
