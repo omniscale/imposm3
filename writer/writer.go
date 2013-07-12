766bc6ebc1c3e6cf2e0b2f09944a1468bdc6e24e
Oliver Tonnhofer <olt@bogosoft.com>
2013-07-12 14:59:51 +0200

    removed old code

--- writer/writer.go
+++ writer/writer.go
@@ -16,39 +16,6 @@ type ErrorLevel interface {
 	Level() int
 }
 
-// type DbWriter struct {
-// 	Db database.DB
-// 	In chan InsertBatch
-// 	wg *sync.WaitGroup
-// }
-
-// func NewDbWriter(db database.DB, in chan InsertBatch) *DbWriter {
-// 	dw := DbWriter{
-// 		Db: db,
-// 		In: in,
-// 		wg: &sync.WaitGroup{},
-// 	}
-// 	for i := 0; i < runtime.NumCPU(); i++ {
-// 		dw.wg.Add(1)
-// 		go dw.loop()
-// 	}
-// 	return &dw
-// }
-
-// func (dw *DbWriter) Close() {
-// 	dw.wg.Wait()
-// }
-
-// func (dw *DbWriter) loop() {
-// 	for batch := range dw.In {
-// 		err := dw.Db.InsertBatch(batch.Table, batch.Rows)
-// 		if err != nil {
-// 			log.Println(err)
-// 		}
-// 	}
-// 	dw.wg.Done()
-// }
-
 type looper interface {
 	loop()
 }
