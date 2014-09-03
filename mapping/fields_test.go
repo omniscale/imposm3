9b3979107667f1a35b17b011cb8611964dad2f70
Oliver Tonnhofer <olt@bogosoft.com>
2014-09-03 08:24:01 +0200

    fix string_suffixreplace test. closes #37

--- mapping/fields_test.go
+++ mapping/fields_test.go
@@ -109,8 +109,8 @@ func TestZOrder(t *testing.T) {
 
 func TestMakeSuffixReplace(t *testing.T) {
 	field := Field{
-		"name", "name", "string_suffixreplace",
-		map[string]interface{}{"suffixes": map[string]interface{}{"Straße": "Str.", "straße": "str."}}}
+		Name: "name", Key: "name", Type: "string_suffixreplace",
+		Args: map[string]interface{}{"suffixes": map[string]interface{}{"Straße": "Str.", "straße": "str."}}}
 	suffixReplace, err := MakeSuffixReplace("name", FieldType{}, field)
 
 	if err != nil {
