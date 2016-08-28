fc361db88af6d72b281aa27b0d9607e054ba4800
lukasmartinelli <me@lukasmartinelli.ch>
2016-08-28 22:15:06 +0200

    Try tile detection for way
    Fix line cover

--- expire/tile.go
+++ expire/tile.go
@@ -12,8 +12,22 @@ type Tile struct {
 	Z int
 }
 
+func (t Tile) toID() int {
+	dim := 2 * (1 << uint(t.Z))
+	return ((dim*t.Y + t.X) * 32) + t.Z
+}
+
 func (t Tile) Key() string {
 	return strings.Join([]string{strconv.Itoa(t.X), strconv.Itoa(t.Y), strconv.Itoa(t.Z)}, "/")
+}
+
+func fromID(id int) Tile {
+	z := id % 32
+	dim := 2 * (1 << uint(z))
+	xy := ((id - z) / 32)
+	x := xy % dim
+	y := ((xy - x) / dim) % dim
+	return Tile{x, y, z}
 }
 
 type TileFraction struct {
@@ -32,6 +46,79 @@ func PointToTileFraction(lon, lat float64, zoomLevel int) TileFraction {
 		Y: z2 * (0.5 - 0.25*math.Log((1+sin)/(1-sin))/math.Pi),
 		Z: zoomLevel,
 	}
+}
+
+type Point struct {
+	lon float64
+	lat float64
+}
+
+func LinestringToTile(points []Point, maxZoom int) []Tile {
+	tiles := []Tile{}
+	var prevX float64
+	var prevY float64
+
+	for i := 0; i < len(points)-1; i++ {
+		start := PointToTileFraction(points[i].lon, points[i].lat, maxZoom)
+		stop := PointToTileFraction(points[i+1].lon, points[i+1].lat, maxZoom)
+
+		x0 := start.X
+		y0 := start.Y
+		x1 := stop.X
+		y1 := stop.Y
+		dx := x1 - x0
+		dy := y1 - y0
+
+		if dy == 0 && dx == 0 {
+			continue
+		}
+
+		sx := -1.0
+		if dx > 0 {
+			sx = 1.0
+		}
+		sy := -1.0
+		if dy > 0 {
+			sy = 1.0
+		}
+
+		x := math.Floor(x0)
+		y := math.Floor(y0)
+
+		tMaxX := math.Abs((x - x0) / dx)
+		if dx > 0 {
+			tMaxX = math.Abs((1 + x - x0) / dx)
+		}
+
+		tMaxY := math.Abs((y - y0) / dy)
+		if dy > 0 {
+			tMaxY = math.Abs((1 + y - y0) / dy)
+		}
+
+		tdx := math.Abs(sx / dx)
+		tdy := math.Abs(sy / dy)
+
+		if x != prevX || y != prevY {
+			tiles = append(tiles, Tile{int(x), int(y), maxZoom})
+			prevX = x
+			prevY = y
+		}
+
+		for tMaxX < 1 || tMaxY < 1 {
+			if tMaxX < tMaxY {
+				tMaxX += tdx
+				x += sx
+			} else {
+				tMaxY += tdy
+				y += sy
+			}
+
+			tiles = append(tiles, Tile{int(x), int(y), maxZoom})
+			prevX = x
+			prevY = y
+		}
+	}
+	return tiles
 }
 
 func PointToTile(lon, lat float64, zoomLevel int) Tile {
--- expire/tile_test.go
+++ expire/tile_test.go
@@ -1,6 +1,9 @@
 package expire
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func comparePointTile(t *testing.T, lon, lat float64, x, y, z int) {
 	expected := Tile{x, y, z}
@@ -10,14 +13,47 @@ func comparePointTile(t *testing.T, lon, lat float64, x, y, z int) {
 	}
 }
 
-func TestKey(t *testing.T) {
-	tile := Tile{8593, 5747, 14}
-	if tile.Key() != "8593/5747/14" {
-		t.Error("Wrong key for tile", tile.Key())
-	}
-}
-
 func TestPointToTile(t *testing.T) {
 	// Knie's Kinderzoo in Rapperswil, Switzerland
 	comparePointTile(t, 8.8223, 47.2233, 8593, 5747, 14)
+}
+
+func TestLongLinestringToTile(t *testing.T) {
+	points := []Point{
+		Point{-106.21719360351562, 28.592359801121567},
+		Point{-106.1004638671875, 28.791130513231813},
+		Point{-105.87661743164062, 28.864519767126602},
+		Point{-105.82374572753905, 28.60743139267596},
+	}
+	tiles := LinestringToTile(points, 10)
+
+	expectedTiles := []Tile{
+		Tile{209, 427, 10},
+		Tile{209, 426, 10},
+		Tile{210, 426, 10},
+		Tile{210, 427, 10},
+	}
+
+	if !reflect.DeepEqual(tiles, expectedTiles) {
+		t.Error("Unexpected tiles", tiles)
+	}
+}
+
+func TestLinestringToTile(t *testing.T) {
+	points := []Point{
+		Point{8.826313018798828, 47.22796198584928},
+		Point{8.82596969604492, 47.20755789924751},
+		Point{8.826141357421875, 47.194845099780174},
+	}
+
+	tiles := LinestringToTile(points, 14)
+	expectedTiles := []Tile{
+		Tile{8593, 5747, 14},
+		Tile{8593, 5748, 14},
+		Tile{8593, 5749, 14},
+	}
+
+	if !reflect.DeepEqual(tiles, expectedTiles) {
+		t.Error("Unexpected tiles", tiles)
+	}
 }
