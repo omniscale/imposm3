698a4fc05cbd7a5bb60cb4bf6b512f0d77e7384e
Oliver Tonnhofer <olt@bogosoft.com>
2017-05-09 11:49:49 +0200

    no need to filter relations without support for old-style multipolygons

--- update/process.go
+++ update/process.go
@@ -350,11 +350,7 @@ func Update(oscFile string, geometryLimiter *limit.Limiter, expireor expire.Expi
 		}
 		// insert new relation
 		progress.AddRelations(1)
-		// filter out unsupported relation types, otherwise they might
-		// get inserted with the tags from an outer way
-		if relTagFilter.Filter(&rel.Tags) {
-			relations <- rel
-		}
+		relations <- rel
 	}
 
 	for wayId, _ := range wayIds {
