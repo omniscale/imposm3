e6b38815e1f20ac1056dc4818f8b57eba97a65f7
Oliver Tonnhofer <olt@bogosoft.com>
2018-04-14 21:51:30 +0200

    travis: build with tag as version

--- version.go
+++ version.go
@@ -1,12 +1,5 @@
 package imposm3
 
-var Version string
-
-// buidVersion gets replaced while building with
-// go build -ldflags "-X github.com/omniscale/imposm3.buildVersion 1234"
-var buildVersion string
-
-func init() {
-	Version = "0.5.0"
-	Version += buildVersion
-}
+// Version gets replaced while building with
+// go build -ldflags "-X github.com/omniscale/imposm3.Version 1.2.3"
+var Version = "0.0.0-dev"
