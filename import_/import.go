99314f350235a64b97ef572d9402473b3c3bc1f2
Oliver Tonnhofer <olt@bogosoft.com>
2018-12-12 09:07:44 +0100

    state: add tests for estimated sequence ID

--- import_/import.go
+++ import_/import.go
@@ -4,14 +4,9 @@ Package import_ provides the import sub command initial imports.
 package import_
 
 import (
-	"fmt"
-	"math"
-	"net/http"
 	"os"
 	"path/filepath"
-	"time"
 
-	"github.com/omniscale/go-osm/parser/pbf"
 	"github.com/omniscale/go-osm/state"
 	"github.com/omniscale/imposm3/cache"
 	"github.com/omniscale/imposm3/config"
@@ -24,7 +19,6 @@ import (
 	"github.com/omniscale/imposm3/stats"
 	"github.com/omniscale/imposm3/update"
 	"github.com/omniscale/imposm3/writer"
-	"github.com/pkg/errors"
 )
 
 func Import(importOpts config.Import) {
@@ -82,7 +76,7 @@ func Import(importOpts config.Import) {
 
 	if importOpts.Read != "" && osmCache.Exists() {
 		if importOpts.Overwritecache {
-			log.Printf("removing existing cache %s", baseOpts.CacheDir)
+			log.Printf("[info] removing existing cache %s", baseOpts.CacheDir)
 			err := osmCache.Remove()
 			if err != nil {
 				log.Fatal("unable to remove cache:", err)
@@ -299,67 +293,4 @@ func Import(importOpts config.Import) {
 
 	step()
 
-}
-
-func estimateFromPBF(filename string, before time.Duration, replicationURL string, replicationInterval time.Duration) (*state.DiffState, error) {
-	f, err := os.Open(filename)
-	if err != nil {
-		return nil, errors.Wrap(err, "opening PBF file")
-	}
-	defer f.Close()
-
-	pbfparser := pbf.New(f, pbf.Config{})
-	header, err := pbfparser.Header()
-
-	var timestamp time.Time
-	if err == nil && header.Time.Unix() != 0 {
-		timestamp = header.Time
-	} else {
-		fstat, err := os.Stat(filename)
-		if err != nil {
-			return nil, errors.Wrapf(err, "reading mod time from %q", filename)
-		}
-		timestamp = fstat.ModTime()
-	}
-
-	if replicationURL == "" {
-		replicationURL = "https://planet.openstreetmap.org/replication/minute/"
-	}
-
-	seq, err := estimateSequence(replicationURL, replicationInterval, timestamp)
-	if err != nil {
-		return nil, errors.Wrap(err, "fetching current sequence for estimated import sequence")
-	}
-
-	// start earlier
-	seq -= int(math.Ceil(before.Minutes() / replicationInterval.Minutes()))
-	return &state.DiffState{Time: timestamp, URL: replicationURL, Sequence: seq}, nil
-}
-
-func currentState(url string) (*state.DiffState, error) {
-	resp, err := http.Get(url + "state.txt")
-	if err != nil {
-		return nil, err
-	}
-	if resp.StatusCode != 200 {
-		return nil, errors.New(fmt.Sprintf("invalid response: %v", resp))
-	}
-	defer resp.Body.Close()
-	return state.Parse(resp.Body)
-}
-
-func estimateSequence(url string, interval time.Duration, timestamp time.Time) (int, error) {
-	state, err := currentState(url)
-	if err != nil {
-		// discard first error and try a second time before failing
-		time.Sleep(time.Second * 2)
-		state, err = currentState(url)
-		if err != nil {
-			return 0, errors.Wrap(err, "fetching current state")
-		}
-	}
-
-	behind := state.Time.Sub(timestamp)
-	// Sequence unit depends on replication interval (minute, hour, day).
-	return state.Sequence - int(math.Ceil(behind.Minutes()/interval.Minutes())), nil
 }
